35968643fa556f6946e34536e4e6041e3324e955
francischen <kfrancischen@gmail.com>
2016-09-22 21:37:35 -0700

    update bit operations

--- bit/operations.py
+++ bit/operations.py
@@ -27,6 +27,14 @@ def setmthBitToZero(m, n):
 def exchange(a, b, x):
     return a ^ b ^ x
 
+# remove the last set bit:
+def removeLastSetBit(n):
+    return n - (n & (-n))
+
+# add 1 to the last set bit: 1011 -> 1100, 1001->1010
+def addOneToLastSetBit(n):
+    return n + (n & (-n))
+
 n = 10
 print deleteLastOne(n)
 print multiplyByTwo(n)
@@ -45,3 +53,7 @@ print setmthBitToZero(m, n)
 x = a = 3
 b = 2
 print exchange(a, b, x)
+
+n = 10
+print removeLastSetBit(n)
+print addOneToLastSetBit(n)
