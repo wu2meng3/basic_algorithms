d17c73526eb21e6fd39c570e19c6ce77dfcc825f
francischen <kfrancischen@gmail.com>
2016-10-18 16:20:48 -0700

    finished file IO

---
+++ basic_operations/fileIO.py
@@ -0,0 +1,25 @@
+
+fileName = 'test.txt'
+
+# read by line
+print "the first"
+with open(fileName) as f:
+    for line in f:
+        print line
+
+# read by bulk
+print "the second"
+with open(fileName) as f:
+    print f.read()
+
+
+
+# read by buffer size:
+print "the third"
+buffersize = 1024
+with open(fileName) as f:
+    while True:
+        buf = f.read(buffersize)
+        if not buf:
+            break
+        print buf
